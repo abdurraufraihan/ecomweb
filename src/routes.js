c9b04538a0f17c18218a367b92c3bfcfb91d7744
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-23 00:40:01 +0600

    home screen initiated and added to route

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './screens/home/homescreen';
 
 export default class Routes extends Component {
 	render() {
 		return (
-			<div>Route Component Initiated</div>
+			<Router>
+				<Switch>
+					<Route path='/' component={Home}/>
+				</Switch>
+			</Router>
 		);
 	}
 }
