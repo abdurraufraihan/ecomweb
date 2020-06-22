19cb6bd173ba99bde2c4a2660a07692a80f8965b
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-23 01:07:17 +0600

    sidebar design completed

--- src/components/sidebar/sidebarview.js
+++ src/components/sidebar/sidebarview.js
@@ -1,9 +1,25 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './sidebarstyle.css';
 
 export default class SideBar extends Component {
 	render() {
 		return (
-			<div>This is side bar</div>
+			<div id='sideBarContainer'>
+				<div id='sideBarBody'>
+					<ul>
+						<Link><li>------------All------------</li></Link>
+						<Link><li>Category Link</li></Link>
+						<Link><li>Category Link</li></Link>
+						<Link><li>Category Link</li></Link>
+						<Link><li>Category Link</li></Link>
+						<Link><li>Category Link</li></Link>
+						<Link><li>Category Link</li></Link>
+						<Link><li>Category Link</li></Link>
+						<Link><li>Category Link</li></Link>
+					</ul>
+				</div>
+			</div>
 		);
 	}
 }
