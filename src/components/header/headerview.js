eecdaf76c6d289557d69c19d6693ea1e3d8c2206
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-23 00:56:05 +0600

    completed header design

--- src/components/header/headerview.js
+++ src/components/header/headerview.js
@@ -1,9 +1,31 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import './headerstyle.css';
 
 export default class Header extends Component {
 	render() {
 		return (
-			<div>This is Header</div>
+			<div id='headerContainer'>
+				<div id='logoContainer'>
+					<div id='burgerIconContainer'>
+						<div className='burgerSlice'></div>
+						<div className='burgerSlice'></div>
+						<div className='burgerSlice'></div>
+					</div>
+					<div id='logo'><Link to='/'>Raihan Store</Link></div>
+				</div>
+				<div id='search'>
+					<form>
+						<input
+							id='searchInput'
+							type='text'
+							placeholder='Search'
+						/>
+						<button type='submit' id='searchButton'>search</button>
+					</form>
+				</div>
+				<div id='signIn'>SIGN IN</div>
+			</div>
 		);
 	}
 }
