61a348ef3f010b24746e638a25143698a83f84e0
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-23 23:55:54 +0600

    Body component initiated and added to Home screen

---
+++ src/components/body/bodyview.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './bodystyle.css'
+
+export default class Body extends Component {
+	render() {
+		return (
+			<div className='bodyContainer'>
+				<div id='body'>
+
+				</div>
+			</div>
+		);
+	}
+}
--- src/screens/home/homescreen.js
+++ src/screens/home/homescreen.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Header from '../../components/header/headerview';
 import SideBar from '../../components/sidebar/sidebarview';
+import Body from '../../components/body/bodyview';
 
 export default class Home extends Component {
 	render() {
@@ -8,6 +9,7 @@ export default class Home extends Component {
 			<React.Fragment>
 				<Header />
 				<SideBar />
+				<Body />
 			</React.Fragment>
 		);
 	}
