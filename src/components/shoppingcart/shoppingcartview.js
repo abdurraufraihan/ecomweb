a784be6e64e811f06ac65c0d3b84d71ebb1ed56f
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-25 23:59:30 +0600

    ShoppingCart component added

---
+++ src/components/shoppingcart/shoppingcartview.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import './shoppingcartstyle.css';
+
+export default class ShoppingCart extends Component {
+	renderEmptyCart = () => {
+		return(
+			<div id='emptyCart'>
+				<i
+					id='shoppingBagIcon'
+					className='fa fa-shopping-basket'
+				></i>
+				<p id='emptyCartText'>Empty Cart</p>
+			</div>
+		);
+	}
+
+	render() {
+		return (
+			<div id='cartContainer' style={this.getCartStyle()}>
+				<div id='cart'>
+					{this.renderEmptyCart()}
+				</div>
+				<div id='orderButtonContainer'>
+					<div id='orderButton'>
+						<div id='placeOrder'>Place Order</div>
+					<div id='orderPrice'>$75</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
