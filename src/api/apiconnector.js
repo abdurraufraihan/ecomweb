8e62618e251a36d3d5bf246e44481de138f394cf
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-28 23:57:50 +0600

    api connector added for all api request

---
+++ src/api/apiconnector.js
@@ -0,0 +1,17 @@
+import ServerUrl from '../lib/serverurl';
+import ApiUtils from './apiutils';
+
+const sendRequest = (relativeUrl, successHandler, erorHandler) => {
+	let url = ServerUrl.BASE_URL + relativeUrl;
+	fetch(url)
+		.then(ApiUtils.statusHandler)
+		.then(ApiUtils.jsonHandler)
+		.then(successHandler)
+		.catch(erorHandler);
+};
+
+const ApiConnector = {
+	sendRequest: sendRequest
+};
+
+export default ApiConnector;
