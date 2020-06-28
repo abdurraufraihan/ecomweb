a12f9588216309c81bd3b2f7d05affd87f4a7e06
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-29 00:24:18 +0600

    category endpoint added to ApiEndpoints

--- src/api/apiendpoints.js
+++ src/api/apiendpoints.js
@@ -1,7 +1,8 @@
 const COMMON_API_PATH = 'api/v1/';
 
 const ApiEndpoints = {
-	PRODUCT_URL: COMMON_API_PATH + 'products'
+	PRODUCT_URL: COMMON_API_PATH + 'products',
+	CATEGORY_URL: COMMON_API_PATH + 'categories'
 };
 
 export default ApiEndpoints;
