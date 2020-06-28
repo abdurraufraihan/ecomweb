46a3df6100720959c043d1a64e991c0453a79121
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-29 00:37:04 +0600

    fetched category from server in sidebar and showed

--- src/components/sidebar/sidebarview.js
+++ src/components/sidebar/sidebarview.js
@@ -1,27 +1,51 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import './sidebarstyle.css';
+import ApiConnector from '../../api/apiconnector';
+import ApiEndpoints from '../../api/apiendpoints';
 
 export default class SideBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			categories: []
+		};
+	}
+
 	getSidebarStyle = () => {
 		return !this.props.isShowSidebar ? {left: '-200px'} : {};
+	}
+
+	getCategoryLink = (categoryId) => `/c/${categoryId}`;
+
+	categorySuccessHandler = (categories) => {
+		this.setState({categories: categories});
+	}
+
+	erorHandler = (error) => {console.error(error)} //TODO:show error right below of header
+
+	componentDidMount() {
+		ApiConnector.sendRequest(
+			ApiEndpoints.CATEGORY_URL,
+			this.categorySuccessHandler,
+			this.errorHandler
+		);
 	}
 
 	render() {
 		return (
 			<div id='sideBarContainer' style={this.getSidebarStyle()}>
 				<div id='sideBarBody'>
-					<ul>
-						<Link><li>------------All------------</li></Link>
-						<Link><li>Category Link</li></Link>
-						<Link><li>Category Link</li></Link>
-						<Link><li>Category Link</li></Link>
-						<Link><li>Category Link</li></Link>
-						<Link><li>Category Link</li></Link>
-						<Link><li>Category Link</li></Link>
-						<Link><li>Category Link</li></Link>
-						<Link><li>Category Link</li></Link>
-					</ul>
+					{this.state.categories &&
+						<ul>
+							<Link to='/'><li>------------All------------</li></Link>
+							{this.state.categories.map((category) => (
+								<Link key={category.id} to={this.getCategoryLink(category.id)}>
+									<li>{category.name}</li>
+								</Link>
+							))}
+						</ul>
+					}
 				</div>
 			</div>
 		);
