b3c2d72c9321b34fdd30793e07a3ab47bd0ca273
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-06-29 23:26:47 +0600

    fix indention of  sendRequest function

--- src/api/apiconnector.js
+++ src/api/apiconnector.js
@@ -4,10 +4,10 @@ import ApiUtils from './apiutils';
 const sendRequest = (relativeUrl, successHandler, erorHandler) => {
 	let url = ServerUrl.BASE_URL + relativeUrl;
 	fetch(url)
-		.then(ApiUtils.statusHandler)
-		.then(ApiUtils.jsonHandler)
-		.then(successHandler)
-		.catch(erorHandler);
+	.then(ApiUtils.statusHandler)
+	.then(ApiUtils.jsonHandler)
+	.then(successHandler)
+	.catch(erorHandler);
 };
 
 const ApiConnector = {
