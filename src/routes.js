fd467599b397f98af52ec75158b468e05f0cf19d
Abdur Rauf Raihan <abdurraufraihan@gmail.com>
2020-09-25 15:42:21 +0600

    common style.css file added to routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import AppPath from './lib/path';
 import HomeScreen from './screens/home/homescreen';
+import './style.css';
 
 export default class Routes extends Component {
 	render() {
